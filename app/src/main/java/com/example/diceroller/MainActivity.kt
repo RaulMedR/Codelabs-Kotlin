e8ea27e239abb57d3d0a86ea443caa3b18e63324
RaulMedR <raul.medina106@alu.ulpgc.es>
2022-10-11 21:29:22 +0100

    Codelab 1.2 Homework

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{ rollDice() }
         val countUpButton: Button = findViewById(R.id.count_up_button)
         countUpButton.setOnClickListener { countUp() }
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener { reset() }
     }
 
     private fun rollDice(){
@@ -28,19 +31,16 @@ class MainActivity : AppCompatActivity() {
     private fun countUp(){
         val resultText: TextView = findViewById(R.id.result_text)
         try {
-            if (Integer.parseInt(resultText.text.toString()) < 6){
+            if (Integer.parseInt(resultText.text.toString()) < 6) {
                 resultText.setText((Integer.parseInt(resultText.text.toString()) + 1).toString())
             }
-
-
-
-
         } catch(err: NumberFormatException){
             resultText.setText("1")
         }
+    }
 
-
-
-
+    private fun reset(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.setText("0")
     }
 }
