12e0e6af01978562df755d2603662a1e416cc19a
RaulMedR <raul.medina106@alu.ulpgc.es>
2022-10-12 14:22:21 +0100

    Codelab 1.3 Challenge

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.diceroller
 
+import android.media.Image
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -9,38 +11,33 @@ import org.w3c.dom.Text
 import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice() }
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        countUpButton.setOnClickListener { countUp() }
-        val resetButton: Button = findViewById(R.id.reset_button)
-        resetButton.setOnClickListener { reset() }
     }
 
     private fun rollDice(){
+        diceImage1.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage() : Int {
         val randomInt = (1..6).random()
-        //Toast.makeText(this, "button clicked",
-        //    Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
-    }
-
-    private fun countUp(){
-        val resultText: TextView = findViewById(R.id.result_text)
-        try {
-            if (Integer.parseInt(resultText.text.toString()) < 6) {
-                resultText.setText((Integer.parseInt(resultText.text.toString()) + 1).toString())
-            }
-        } catch(err: NumberFormatException){
-            resultText.setText("1")
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        return drawableResource
     }
 
-    private fun reset(){
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.setText("0")
-    }
 }
